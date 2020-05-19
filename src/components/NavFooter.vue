129f923bea1b4bcbc0817be595abd8f3d1d8427f
aloe_hhy <hhy0805@foxmail.com>
2020-05-19 10:58:44 +0800

    navfooter

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,18 @@
 <template>
-  <div></div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="javascript">aloe_web</a><span>|</span>
+      <a href="javascript">HTML5</a><span>|</span>
+      <a href="javascript">javaScript</a><span>|</span>
+      <a href="javascript">css</a><span>|</span>
+      <a href="javascript">vue</a>
+    </div>
+    <div class="copyright">Copyright©<span class="domian">mi.com  京ICP证110507号 京ICP备10046444号 京公网安备11010802020134号 京网文[2020]0276-042号</span></div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +20,38 @@ export default {
   name: 'nav-footer'
 }
 </script>
+
+<style lang="scss">
+.footer{
+  height: 234px;
+  border-top: 4px solid #FF6600;
+  background-color: #333333;
+  color: #999999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo{
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img{
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link{
+    margin-bottom: 13px;
+    a{
+      color: #999999;
+      display: inline-block;
+    }
+    span{
+      margin: 0 10px;
+    }
+  }
+  .copyright{
+    .domain{
+      color: #FF6600;
+    }
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -11,36 +11,37 @@
 // changeOrigin 路径设置位原点
 // pathRewrite 将后面的地址接在target的地址上
 
-// module.exports = {
-//   devServer: {
-//     host:'localhost',
-//     port:8080,
-//     proxy:{
-//       './api':{
-//         target:'https://wwww.imooc.com',
-//         changeOrigin:false,
-//         pathRewrite:{
-//           '/api':''
-//         }
-//       }
-//     },
- 
-//   clientLogLevel: 'debug'
- 
- 
-//   }
-//  }
 module.exports = {
   devServer: {
-    host: 'localhost',
-    port: 8080,
-    '/api':{
-      target:'http://www.imooc.com',
-      changeOrigin:false,
-      pathRewrite:{
-                  '/api':''
-                }
-    }
+    host:'localhost',
+    port:8080,
+    proxy:{
+      './api':{
+        target:'https://wwww.imooc.com',
+        changeOrigin:false,
+        pathRewrite:{
+          '/api':''
+        }
+      }
+    },
+ 
+  // clientLogLevel: 'debug'   //none | error | warning | info(info默认)
+  clientLogLevel: 'info'
+ 
+ 
   }
-}
+ }
+// module.exports = {
+//   devServer: {
+//     host: 'localhost',
+//     port: 8080,
+//     '/api':{
+//       target:'http://www.imooc.com',
+//       changeOrigin:false,
+//       pathRewrite:{
+//                   '/api':''
+//                 }
+//     }
+//   }
+// }
  
